d13f5e89ed51cf0dd84d1ec6ccd4de805ebe7fb7
Dylan Harrison <dillysnaps@gmail.com>
2017-12-23 21:50:50 -0600

    login-setup

--- app.js
+++ app.js
@@ -9,11 +9,56 @@ app.get('/api', (req, res) => {
     });
 });
 
-app.post('/api/posts', (req, res) => {
-    res.json({
-        message: 'Post created...'
+app.post('/api/posts', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if(err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                message: 'Post created...',
+                authData
+            });
+        }
     });
 });
+
+app.post('/api/login', (req, res) => {
+
+    //mock user
+    const user = {
+        id: 1,
+        username: 'dylan',
+        email: 'dylan@email.com'
+    }
+
+    jwt.sign({user: user}, 'secretkey', (err, token) => {
+        res.json({token});
+    });
+});
+
+// format of token
+// Authorization: Bearer <access_token>
+
+//verify token
+function verifyToken(req, res, next) {
+    // get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // check if bearer is undefined
+    if (typeof bearerHeader !== 'undefined') {
+
+        // split at the space
+        const bearer = bearerHeader.split(' ');
+        //get token from array
+        const bearerToken = bearer[1];
+        // set token
+        req.token = bearerToken;
+        // next middleware
+        next();
+
+    } else {
+        res.sendStatus(403);
+    }
+}
 
 app.listen(5000, () => {
     console.log('Server started on port 5000');
